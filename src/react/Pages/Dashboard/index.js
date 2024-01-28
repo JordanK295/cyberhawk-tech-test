a1628244e359963d1b42c07d69c7e3ce0ee7d757
JordanK295 <jkay10100@gmail.com>
2024-01-28 12:03:20 +0000

    feat: Dashboard search

--- src/react/Pages/Dashboard/index.js
+++ src/react/Pages/Dashboard/index.js
@@ -1,8 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+// import getFarms from '../../Services/getFarms';
+
 function Dashboard() {
-  console.log('dashboard');
+  const [farms, setFarms] = useState([]);
+  const [error, setError] = useState('');
+  // const [singleSelections, setSingleSelections] = useState([]);
+
+  // const listbox = {
+  //   data: ['Peach', 'Pear', 'Pineapple', 'Plum', 'Pomegranate', 'Prune'],
+  // };
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/farms')
+      .then((res) => setFarms(res.data.data))
+      .catch((err) => {
+        setError(err.message);
+      });
+  }, []);
+
+  // console.log('FFf', farms, singleSelections);
+
+  const formatResult = (item) => (
+    <>
+      {/* <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span> */}
+      <span className="cursor-pointer">{item.name}</span>
+    </>
+  );
+
   return (
-    <div>
-      <p>Dashboard</p>
+    <div className="w-full h-screen text-neutral-700">
+      <header className="flex justify-between bg-neutral-100 h-16 items-center px-4 py-2">
+        <p>LOGO</p>
+        <div className="w-72">
+          <ReactSearchAutocomplete
+            items={farms}
+            // onSearch={handleOnSearch}
+            // onHover={handleOnHover}
+            // onSelect={handleOnSelect}
+            // onFocus={handleOnFocus}
+            // autoFocus
+            formatResult={formatResult}
+          />
+        </div>
+      </header>
+      <div className="rounded m-4">
+        <h1>Farms</h1>
+        <h2>Recently Viewed</h2>
+        {error && <p className="text-danger">{error}</p>}
+        {farms && farms.length > 0 && (
+          <ul className="flex justify-evenly">
+            {farms.slice(0, 4).map((user) => (
+              <div>
+                <li className="px-4 py-2 rounded-lg bg-neutral-100" key={user.id}>
+                  {user.name}
+                </li>
+              </div>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
